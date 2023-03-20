4e92ba539082d62de05f2a2d0cd44ee298ee2ab5
Gergo Kovacs <kgerg0017@gmail.com>
2023-03-20 20:38:23 +0100

    Win and tie function added

--- script.js
+++ script.js
@@ -6,20 +6,62 @@ let result = document.getElementById("result");
 let X_PLAYER = "X";
 let O_PLAYER = "O";
 let currentPlayer = X_PLAYER;
+let moves = 0;
 
 let hiddenBoard = Array(9).fill(null);
 
+// 0 1 2
+// 3 4 5
+// 6 7 8
+
+const winnerCombinations = [
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [6,4,2]
+]
+
 const startGame = () => {
-    boxes.forEach(box => box.addEventListener("click", clickonBox))
+    boxes.forEach(box => box.addEventListener("click", clickonBox));
 }
 
 function clickonBox(e){
     const boxID = e.target.id;
-    e.target.innerText = currentPlayer;
+
+    if(hiddenBoard[boxID] == null){
+        hiddenBoard[boxID] = currentPlayer;
+        e.target.innerText = currentPlayer;
+        moves++;
+
+       
+        if(playerWins() != false){
+            result.innerText = currentPlayer + "wins";
+                }
+        else if(moves == 9){
+            result.innerText = "It is a TIE!"
+        }
+        
+    }
+
+
+    currentPlayer = currentPlayer == X_PLAYER ? O_PLAYER : X_PLAYER;
+    
 }
 
 function playerWins(){
-
+    for(let winArray of winnerCombinations){
+        let [x,y,z] = winArray;
+        // 
+        let winner = hiddenBoard[x] && (hiddenBoard[x] == hiddenBoard[y] && hiddenBoard[x]) == hiddenBoard[z];
+        if(winner){
+            return[x,y,z];
+        }
+    }
+    return false;
 }
 
 restartBtn.addEventListener("click", restartGame);
@@ -29,6 +71,10 @@ function restartGame(){
     boxes.forEach( box => {
         box.innerText = "";
     })
+    
+    moves = 0;
+    result.innerText = "";
+    currentPlayer = X_PLAYER
 }
 
 startGame();
