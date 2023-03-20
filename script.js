5acad58c0f4cb32725339812e1232d104238753d
Gergo Kovacs <kgerg0017@gmail.com>
2023-03-20 19:44:45 +0100

    Basic logic

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ let result = document.getElementById("result");
 let X_PLAYER = "X";
 let O_PLAYER = "O";
 let currentPlayer = X_PLAYER;
+
+let hiddenBoard = Array(9).fill(null);
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener("click", clickonBox))
+}
+
+function clickonBox(e){
+    const boxID = e.target.id;
+    console.log(boxID);
+}
+
+function playerWins(){
+    
+}
+
+restartBtn.addEventListener("click", restartGame);
+
+function restartGame(){
+
+}
+
+startGame();
